331a862f605f32a24f81c62f581d535dc9abd7d8
Dan Crosby <dan@danielcrosby.net>
2022-12-06 07:41:27 -0800

    working sync service

--- LifespanTester/PhoneSyncPeripheral.swift
+++ LifespanTester/PhoneSyncPeripheral.swift
@@ -9,34 +9,78 @@ import Foundation
 import CoreBluetooth
 
 class PhoneSyncPeripheral: NSObject, CBPeripheralManagerDelegate {
-    let serviceUuidString = "b0779ed8-f74a-44f5-a9bf-eeb0c76a502e"
-    let characteristicUuidString = "1bc63fa8-b79d-461c-8cf3-839fc5fba809"
+    let serviceUUID = CBUUID(string: "b0779ed8-f74a-44f5-a9bf-eeb0c76a502e")
+    let characteristicUUID = CBUUID(string: "1bc63fa8-b79d-461c-8cf3-839fc5fba809")
     
-    var mainCharacteristic: CBMutableCharacteristic!
-    var peripheral: CBPeripheralManager!
+    var transferService: CBMutableService!
+    var transferCharacteristic: CBMutableCharacteristic!
+    var peripheralManager: CBPeripheralManager!
     
     var value: Data? {
         get {
-            return mainCharacteristic.value
+            return transferCharacteristic.value
         }
         set(newValue) {
-            mainCharacteristic.value = newValue
-            peripheral.updateValue(newValue ?? Data(), for: mainCharacteristic, onSubscribedCentrals: nil)
+            transferCharacteristic.value = newValue
+            peripheralManager.updateValue(newValue ?? Data(), for: transferCharacteristic, onSubscribedCentrals: nil)
         }
     }
     
+    private func setUpPeripheral() {
+        let transferCharacteristic = CBMutableCharacteristic(type: characteristicUUID,
+                                                             properties: [.notify, .writeWithoutResponse],
+                                                             value: nil,
+                                                             permissions: [.readable, .writeable])
+        
+        let transferService = CBMutableService(type: serviceUUID, primary: true)
+        transferService.characteristics = [transferCharacteristic]
+        peripheralManager.add(transferService)
+        
+        self.transferService = transferService
+        self.transferCharacteristic = transferCharacteristic
+    }
+    
+    private func stopPeripheral() {
+        peripheralManager.removeAllServices()
+    }
+    
     func peripheralManagerDidUpdateState(_ peripheral: CBPeripheralManager) {
-        print("HERE")
+        self.peripheralManager = peripheral
         
-        let mainService = CBMutableService(type: CBUUID(string: serviceUuidString), primary: true)
-        mainCharacteristic = CBMutableCharacteristic(type:CBUUID(string:characteristicUuidString), properties:[.notify, .write, .writeWithoutResponse], value:nil, permissions:[.readable])
-        mainService.characteristics = [mainCharacteristic]
-        
-        peripheral.add(mainService)
-        peripheral.startAdvertising(
-            [CBAdvertisementDataLocalNameKey : "PSP",
-           CBAdvertisementDataServiceUUIDsKey: [serviceUuidString],
-  CBAdvertisementDataSolicitedServiceUUIDsKey: [serviceUuidString]])
+        switch (peripheral.state) {
+            
+        case .unknown:
+            print("updated to Unknown state")
+            
+        case .resetting:
+            print("updated to Resetting state")
+            
+        case .unsupported:
+            print("updated to Unsupported state")
+            
+        case .unauthorized:
+            print("updated to Unauthorized state")
+            
+        case .poweredOff:
+            print("updated to PoweredOff state")
+            
+        case .poweredOn:
+            print("updated to PoweredOn state")
+            setUpPeripheral()
+            
+        @unknown default:
+            break
+            
+        }
+    }
+    
+    func peripheralManager(_ peripheral: CBPeripheralManager, didAdd service: CBService, error: Error?) {
+        print("Starting to advertise with error \(String(describing: error))")
+        peripheral.startAdvertising([CBAdvertisementDataServiceUUIDsKey: [service.uuid]])
+    }
+    
+    func peripheralManagerDidStartAdvertising(_ peripheral: CBPeripheralManager, error: Error?) {
+        print("Started advertising with error \(String(describing: error))")
     }
     
     func peripheralManager(_ peripheral: CBPeripheralManager, didReceiveWrite requests: [CBATTRequest]) {
@@ -44,7 +88,7 @@ class PhoneSyncPeripheral: NSObject, CBPeripheralManagerDelegate {
             print("did receive write for characteristic \(request.characteristic.uuid.uuidString)")
             print("value is \(String(describing: request.value?.hexEncodedString()))")
         }
-        
+
     }
     
     func peripheralManager(_ peripheral: CBPeripheralManager, didReceiveRead request: CBATTRequest) {
@@ -54,5 +98,13 @@ class PhoneSyncPeripheral: NSObject, CBPeripheralManagerDelegate {
     
     func peripheralManager(_ peripheral: CBPeripheralManager, central: CBCentral, didSubscribeTo characteristic: CBCharacteristic) {
         print("did subscribe to \(characteristic.uuid.uuidString)")
+    }
+    
+    func peripheralManager(_ peripheral: CBPeripheralManager, didOpen channel: CBL2CAPChannel?, error: Error?) {
+        print("opened channel")
+    }
+    
+    func peripheralManagerIsReady(toUpdateSubscribers peripheral: CBPeripheralManager) {
+        print("ready to update")
     }
 }
