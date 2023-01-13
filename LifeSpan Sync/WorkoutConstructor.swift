71eead41140a831d9e21911c43d9a4067cf708a5
nolen777 <dan@danielcrosby.net>
2023-01-12 20:06:40 -0800

    sync calories and distance (#11)

--- LifeSpan Sync/WorkoutConstructor.swift
+++ LifeSpan Sync/WorkoutConstructor.swift
@@ -10,13 +10,37 @@ import HealthKit
 
 class WorkoutConstructor {
     let store = HKHealthStore()
+    let stepCountType: HKQuantityType!
+    let calorieCountType: HKQuantityType!
+    let distanceType: HKQuantityType!
     
     init() {
-        DispatchQueue.main.async {
-            let workoutStatus = self.store.authorizationStatus(for: HKWorkoutType.workoutType())
-            let stepStatus = self.store.authorizationStatus(for: HKObjectType.quantityType(forIdentifier: .stepCount)!)
-            if workoutStatus == .notDetermined || stepStatus == .notDetermined {
-                self.store.requestAuthorization(toShare: [HKWorkoutType.workoutType(), HKObjectType.quantityType(forIdentifier: .stepCount)!], read: nil) { (success, error) -> Void in
+        guard let stepCountType = HKObjectType.quantityType(forIdentifier: HKQuantityTypeIdentifier.stepCount) else {
+            fatalError("Unable to create a step count type")
+        }
+        self.stepCountType = stepCountType
+        
+        guard let calorieCountType = HKObjectType.quantityType(forIdentifier: HKQuantityTypeIdentifier.activeEnergyBurned) else {
+            fatalError("Unable to create calorie count type")
+        }
+        self.calorieCountType = calorieCountType
+        
+        guard let distanceType = HKObjectType.quantityType(forIdentifier: HKQuantityTypeIdentifier.distanceWalkingRunning) else {
+            fatalError("Unable to create distance type")
+        }
+        self.distanceType = distanceType
+        
+        DispatchQueue.main.async { [self] in
+            let workoutStatus = store.authorizationStatus(for: HKWorkoutType.workoutType())
+            let stepStatus = store.authorizationStatus(for: stepCountType)
+            let calorieStatus = store.authorizationStatus(for: calorieCountType)
+            let distanceStatus = store.authorizationStatus(for: distanceType)
+            
+            if workoutStatus == .notDetermined ||
+                stepStatus == .notDetermined ||
+                calorieStatus == .notDetermined ||
+                distanceStatus == .notDetermined {
+                store.requestAuthorization(toShare: [HKWorkoutType.workoutType(), stepCountType, calorieCountType, distanceType], read: nil) { (success, error) -> Void in
                     guard success else {
                         fatalError("Failed to authorize HealthKit access with error \(String(describing: error))")
                     }
@@ -63,19 +87,20 @@ class WorkoutConstructor {
                                    totalDistance: distance,
                                    metadata: nil)
         
-        store.save(indoorWalk) { (success, error) -> Void in
+        store.save(indoorWalk) { [self] (success, error) -> Void in
             guard success else {
                 print("Failed to save walk with error \(String(describing: error))")
                 return
             }
             
-            guard let stepCountType = HKObjectType.quantityType(forIdentifier: HKQuantityTypeIdentifier.stepCount) else {
-                fatalError("Unable to create a step count type")
-            }
             let stepQuantity = HKQuantity(unit: HKUnit.count(), doubleValue: Double(stepCount))
             let stepSample = HKQuantitySample(type: stepCountType, quantity: stepQuantity, start: startDate, end: endDate)
             
-            self.store.add([stepSample], to: indoorWalk) { (success, error) -> Void in
+            let activeEnergySample = HKQuantitySample(type: calorieCountType, quantity: energyBurned, start: startDate, end: endDate)
+            
+            let distanceSample = HKQuantitySample(type: distanceType, quantity: distance, start: startDate, end: endDate)
+            
+            store.add([stepSample, activeEnergySample, distanceSample], to: indoorWalk) { (success, error) -> Void in
                 guard success else {
                     print("Failed to add steps with error \(String(describing: error))")
                     return
